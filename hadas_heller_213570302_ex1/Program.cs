e58949ddfa77c857d7f60bc6be58840707311b8b
hadas heller <handesaim3a9@mbjcomp.org.il>
2024-03-21 15:42:10 +0200

    add infinit loop with switch case add segment a in ex

--- hadas_heller_213570302_ex1/Program.cs
+++ hadas_heller_213570302_ex1/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace MyApp
+
 {
     internal class Program
     {
+        static void getWidthAndHeight(ref int width, ref int height)
+        {
+            Console.WriteLine("enter width");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter height");
+            height = Convert.ToInt32(Console.ReadLine());
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("bye bye");
+            int answer, height=0,width=0;
+            for (; ; )
+            {
+                Console.WriteLine("Please type your choice; \n1 to rectange tower \n2 to tringe tower");
+                answer = Convert.ToInt32(Console.ReadLine());
+                switch (answer)
+                {
+                    case 1:
+                        getWidthAndHeight(ref width, ref height);
+                        if(width== height||Math.Abs(width - height) > 5)
+                        {
+                            Console.WriteLine(height * width);
+                        }
+                        else
+                        {
+                            Console.WriteLine(width * 2 + height * 2);
+                        }
+                        //Console.WriteLine("rectange tower\n " + width + "*" + height);
+                        break;
+                    case 2:
+                        getWidthAndHeight(ref width, ref height);
+                        Console.WriteLine("triangle tower\n"+width+ "*"+height);
+                        break;
+                    case 3:
+                        Console.WriteLine("bye bye");
+                        return;
+                    default:
+                        Console.WriteLine("wrong input\n");
+                        break;
+                }
+            }
         }
     }
 }
