7bb4f3c9033f84cc9aa68e08972f701b90ecc09c
hadas heller <handesaim3a9@mbjcomp.org.il>
2024-03-21 16:39:57 +0200

    add triangleTower function

--- hadas_heller_213570302_ex1/Program.cs
+++ hadas_heller_213570302_ex1/Program.cs
@@ -4,8 +4,42 @@ using System.Linq.Expressions;
 namespace MyApp
 
 {
+
     internal class Program
     {
+        static void rectangeTower(int width, int height)
+        {
+            if (width == height || Math.Abs(width - height) > 5)
+            {
+                Console.WriteLine("the area is " + height * width + "\n");
+            }
+            else
+            {
+                Console.WriteLine("the perimeter is " + ((width * 2) + (height * 2)) + "\n");
+            }
+        }
+        static void triangleTower(int width,int height)
+        {
+            int answer=0;
+            while (answer != 1 && answer != 2) {
+                Console.WriteLine("1 for triangle perimeter\n2 for printing the triangle");
+                answer = Convert.ToInt32(Console.ReadLine());
+
+            }
+            if (answer == 1)
+            {
+                Console.WriteLine(width + Math.Sqrt(Math.Pow(width, 2) + 4 * Math.Pow(height, 2))+"\n");
+            }
+            else
+            {
+                if (width % 2 == 0 || width > height * 2) { Console.WriteLine("the triangle is unprintable\n"); }
+                else
+                {
+                    Console.WriteLine("*\n**");
+                }
+
+            }
+        }
         static void getWidthAndHeight(ref int width, ref int height)
         {
             Console.WriteLine("enter width");
@@ -21,26 +55,18 @@ namespace MyApp
             {
                 Console.WriteLine("Please type your choice; \n1 to rectange tower \n2 to tringe tower");
                 answer = Convert.ToInt32(Console.ReadLine());
+                //fix case when a string is typed
                 switch (answer)
                 {
                     case 1:
                         getWidthAndHeight(ref width, ref height);
-                        if(width== height||Math.Abs(width - height) > 5)
-                        {
-                            Console.WriteLine(height * width);
-                        }
-                        else
-                        {
-                            Console.WriteLine(width * 2 + height * 2);
-                        }
-                        //Console.WriteLine("rectange tower\n " + width + "*" + height);
+                        rectangeTower(width, height);
                         break;
                     case 2:
                         getWidthAndHeight(ref width, ref height);
-                        Console.WriteLine("triangle tower\n"+width+ "*"+height);
+                        triangleTower(width, height);
                         break;
                     case 3:
-                        Console.WriteLine("bye bye");
                         return;
                     default:
                         Console.WriteLine("wrong input\n");
